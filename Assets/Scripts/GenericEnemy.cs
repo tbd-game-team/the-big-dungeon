98e166bd0dff81c1ccc4ff78e3211ed558d42ba5
Niklas J. Nett <niklas.j.nett@disroot.org>
2022-08-10 18:23:02 +0200

    Add attack

--- Assets/Scripts/GenericEnemy.cs
+++ Assets/Scripts/GenericEnemy.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class GenericEnemy : MonoBehaviour
 {
-    [SerializeField] private float speed = 7.5f;
-    [SerializeField] private GameObject projectilePrefab;
+    [Header("Movement")]
+    /// <summary>
+    /// Character Walk Speed
+    /// </summary>
+    [SerializeField]
+    private float speed = 3.5f;
 
-    private Animator characterAnimator;
+    [Header("Movement")]
+    [SerializeField]
+    private GameObject target;
+
+    [Header("Combat")]
+    [SerializeField]
+    private GameObject projectilePrefab;
+
+    [Header("Combat")]
+    [SerializeField]
+    private float fireCooldown = .5f;
+
+    private float nextFire = .0f;
+
     void Start()
     {
-
     }
 
     void FixedUpdate()
     {
+        var dir = transform.LookAt(target.transform);
         var x = 1;
         var y = 1;
 
         handleAnimation(x, y);
         handleMovement(x, y);
+        handleCombat(x, y);
     }
 
     void handleAnimation(float x, float y)
@@ -37,5 +55,16 @@ public class GenericEnemy : MonoBehaviour
     void handleMovement(float x, float y)
     {
         transform.Translate(new Vector3(x, y, 0).normalized * Time.deltaTime * speed);
+    }
+
+    void handleCombat()
+    {
+        float distance = Vector3.Distance(object1.transform.position, object2.transform.position);
+
+        if (distance < 100 && Time.time >= nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        }
     }
 }
