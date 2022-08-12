924f93e6e1b948c8f4c1f760d4aeab7d55276a8b
Niklas J. Nett <niklas.j.nett@disroot.org>
2022-08-12 23:22:28 +0200

    Enable kinematic physics

--- Assets/Scripts/GenericProjectile.cs
+++ Assets/Scripts/GenericProjectile.cs
@@ -3,32 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
-[RequireComponent(typeof(Rigidbody2D))]
 public class GenericProjectile : MonoBehaviour
 {
 
     [SerializeField]
     private float speed = 40f;
 
-    private Rigidbody2D rb;
     public GameObject explosion;
     private Vector2 screenBounds;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-    }
-
-    void Start()
-    {
-        rb.mass = 0;
-        rb.velocity = new Vector2(speed, 0);
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(speed, 0);
+        transform.position += transform.right * speed * Time.deltaTime;
+
         Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
         if (viewPos.x < -.1 || viewPos.x > 1.1 || viewPos.y < -.1 || viewPos.y > 1.1)
         {
