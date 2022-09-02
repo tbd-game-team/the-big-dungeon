6a5ada04c412ab8073815284f060219d0bee2ad8
Shaquiri1 <florian.weber261199@gmail.com>
2022-09-02 15:30:14 +0200

    Animated Health bar

--- Assets/Scripts/HealthUi.cs
+++ Assets/Scripts/HealthUi.cs
@@ -11,9 +11,13 @@ public class HealthUi : MonoBehaviour
     
     
     private Image[] hearts;
+    private int maxHealth = 5;
+
+    private int oldHealth;
 
 
     private void Start() {
+        oldHealth = maxHealth;
         hearts = gameObject.GetComponentsInChildren<Image>();
         foreach(Image heart in hearts){
             heart.sprite = fullHeart;
@@ -21,7 +25,7 @@ public class HealthUi : MonoBehaviour
     }
 
     public void updateHearts(int currentHealth){
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < maxHealth; i++){
             if(i<currentHealth){
                 hearts[i].sprite = fullHeart;
             }else{
