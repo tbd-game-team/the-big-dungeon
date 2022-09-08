2fee445778d570b917ea605dc382e7631bfb3cdd
Niklas J. Nett <niklas.j.nett@disroot.org>
2022-09-08 18:37:28 +0200

    Add docs

--- Assets/Scripts/Combat/EnemySpawner.cs
+++ Assets/Scripts/Combat/EnemySpawner.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public static class EnemySpawner
 {
+    /// <summary>
+    /// Spawn the initially in the level placed enemies.
+    /// This information is taken from the ActorGenerator.
+    /// </summary>
     public static void SpawnStarterEnemies()
     {
         var locations = ActorGenerator.GetEnemyPositions();
@@ -11,14 +15,25 @@ public static class EnemySpawner
         foreach (var loc in locations)
         {
             var prefabName = "Enemy";
-            var prefab = Resources.Load(prefabName);
-            var enemy = Object.Instantiate(prefab, loc, Quaternion.identity) as GameObject;
 
-            var enemyController = enemy.GetComponent<GenericEnemy>();
-            enemyController.target = GameObject.FindWithTag("Player");
-
-
-            Debug.Log("Enemy Positions: (" + loc.x + ", " + loc.y + ")");
+            SpawnEnemy(prefabName, loc);
         }
     }
+
+    /// <summary>
+    /// Spawns a single enemy. The enemy class can only be recognized if the file is put correctly.
+    /// </summary>
+    /// <param name="prefabName">Name of the enemy prefab resource.</param>
+    /// <param name="loc">Where the enemy is to be spawned.</param>
+    public static void SpawnEnemy(string prefabName, Vector3 loc)
+    {
+        var locations = ActorGenerator.GetEnemyPositions();
+
+        var prefab = Resources.Load(prefabName);
+        var enemy = Object.Instantiate(prefab, loc, Quaternion.identity) as GameObject;
+
+        var enemyController = enemy.GetComponent<GenericEnemy>();
+        enemyController.target = GameObject.FindWithTag("Player");
+    }
+
 }
