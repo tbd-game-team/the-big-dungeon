ea3636015142ecdf913ee3c2ec86b9454a34f7f4
Niklas J. Nett <niklas.j.nett@disroot.org>
2022-09-08 18:13:52 +0200

    Instantiate enemy dynamically

--- Assets/Scripts/Combat/EnemySpawner.cs
+++ Assets/Scripts/Combat/EnemySpawner.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public static class EnemySpawner
 {
-    public static GameObject enemyPrefab;
-
     public static void SpawnStarterEnemies()
     {
         var locations = ActorGenerator.GetEnemyPositions();
 
         foreach (var loc in locations)
         {
-            var enemy = Object.Instantiate(enemyPrefab);
-            enemy.transform.position = loc;
+            var pathOfPrefabDirectory = "Prefabs/Combat";
+            var prefabName = "Enemy";
+
+            var prefab = Resources.Load(pathOfPrefabDirectory + prefabName, typeof(GameObject)) as GameObject;
+            var enemy = Object.Instantiate(prefab, loc, Quaternion.identity);
+
             var enemyController = enemy.GetComponent<GenericEnemy>();
             enemyController.target = GameObject.FindWithTag("Player");
+
 
             Debug.Log("Enemy Positions: (" + loc.x + ", " + loc.y + ")");
         }
