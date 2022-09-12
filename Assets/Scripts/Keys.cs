18789df4388ca2b4c2effa5e05533ff26f3a2280
Niklas J. Nett <niklas.j.nett@disroot.org>
2022-09-13 00:55:58 +0200

    Move static keys

--- Assets/Scripts/Keys.cs
+++ Assets/Scripts/Keys.cs
@@ -1,8 +1,11 @@
-public static class Keys
+namespace Assets.Scripts
 {
-    public const string ANIMATION_SPEED_KEY = "Speed";
-    public const string ANIMATION_CRACK_KEY = "Crack";
-    public const string TAG_PROJECTILE = "Projectile";
-    public const string TAG_ENEMY = "Enemy";
-    public const string TAG_PLAYER = "Player";
+    public static class Keys
+    {
+        public const string ANIMATION_SPEED_KEY = "Speed";
+        public const string ANIMATION_CRACK_KEY = "Crack";
+        public const string TAG_PROJECTILE = "Projectile";
+        public const string TAG_ENEMY = "Enemy";
+        public const string TAG_PLAYER = "Player";
+    }
 }
